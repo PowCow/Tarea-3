ffd0f2ba0223245444176cfa8f4f03cc621919d1
Your Name <you@example.com>
2025-05-23 11:24:20 -0400

    hola

--- Tarea3.c
+++ Tarea3.c
@@ -9,29 +9,40 @@ typedef struct {
     int peso ;
     int valor ;
 } Item ;
+
 typedef struct {
     int ID ;
     char nombre[50] ;
     char descripcion[200] ;
-    List* items ;
-    int conexiones[4] ;
+    Item items ;
+    int arriba ;
+    int abajo ;
+    int izquierda ; 
+    int derecha ;
     int es_final ;
 } Escenario ;
 
-void leer_escenarios() { //carga canciones del archivo csv
+void leer_escenarios(Escenario *escenarios, int *cantEscenarios) { //carga canciones del archivo csv
+    
     FILE *archivo = fopen("graphquest.csv", "r") ;
     if (archivo == NULL) {
-    perror("Error al abrir el archivo") ; // Informa si el archivo no puede abrirse
-    return;
+        perror("Error al abrir el archivo") ; // Informa si el archivo no puede abrirse
+        return;
     }
-
     char **campos ;
-    // Leer y parsear una línea del archivo CSV. La función devuelve un array de
-    // strings, donde cada elemento representa un campo de la línea CSV procesada.
     campos = leer_linea_csv(archivo, ',') ;// Lee los encabezados del CSV
 
     // Lee cada línea del archivo CSV hasta el final
     while ((campos = leer_linea_csv(archivo, ',')) != NULL) {
+
+        Escenario *lugar = &escenarios[*cantEscenarios] ; 
+        lugar->ID = atoi(campos[0]) ;
+        
+
+
+
+
+
         printf("ID: %d\n", atoi(campos[0])) ;
         printf("Nombre: %s\n", campos[1]) ;
         printf("Descripcion: %s\n", campos[2]) ;
@@ -61,6 +72,7 @@ void leer_escenarios() { //carga canciones del archivo csv
 
         int is_final = atoi(campos[8]) ;
         if (is_final) printf("Es final\n") ;
+        puts("") ; // Imprime una linea en blanco para separar los escenarios
 
         list_clean(items) ;
         free(items) ;
@@ -80,7 +92,11 @@ void leer_escenarios() { //carga canciones del archivo csv
 
 
 int main() {
-  leer_escenarios();
+    Escenario escenario ;
+    int cantidad_escenarios = 0 ;
 
-  return 0;
+    leer_escenarios(escenario, &cantidad_escenarios) ;
+    mostrarEscenarios(escenario, cantidad_escenarios) ;
+
+    return 0;
 }
