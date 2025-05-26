367c38fa999e28aeacc5927b14a95579c963b11b
PowCow <paulina.munoz.c01@mail.pucv.cl>
2025-05-25 23:08:58 -0400

    comentarios... LISTOS!

--- Tarea3.c
+++ Tarea3.c
@@ -5,28 +5,28 @@
 #include "tdas/map.h"
 #include "tdas/extra.h"
 
-typedef struct {
+typedef struct { //estructura sobre los datos de los items encontrados en cada escenario
     char* nombreItem ;
     int pesoItem ;
     int valorItem ;
 } Item ;
-typedef struct {
+typedef struct { //estructura que tiene los datos de un escenario
     int ID ;
     char* nombreEscenario ;
-    char* descripcion ;
-    List* items ;
+    char* descripcion ; //explica el como se ve el escenario 
+    List* items ; //contiene los items que se encuentran en el escenario
     int arriba, abajo, izquierda, derecha ; //movimientos posibles
     int esFinal ;
 } Escenario ;
-typedef struct {
+typedef struct { //estructura que contiene los datos del jugador
     Escenario* actual ; //escenario en el que se encuentra el jugador
     List* inventario ; //lista de items que ha recogido el jugador
-    float tiempoRestante ;
+    float tiempoRestante ; //tiempo que le queda al jugador
     int puntajeAcumulado ;
-    int pesoTotal ;
+    int pesoTotal ; //suma del peso de los items que tenga el jugador
 } Jugador ;
 
-Item* crearItem(char* nombre, int valor, int peso) {
+Item* crearItem(char* nombre, int valor, int peso) { //reserva memoria para guardar (crear) un nuevo item
     Item* nuevito = malloc(sizeof(Item)) ;
     nuevito->nombreItem = strdup(nombre) ;
     nuevito->valorItem = valor ;
@@ -34,20 +34,20 @@ Item* crearItem(char* nombre, int valor, int peso) {
     return nuevito ;
 }
 
-int compararEnteros(void* a, void* b) { //Listo
+int compararEnteros(void* a, void* b) { //compara dos claves 'int' almacenadas en un mapa para que este pueda buscar, insertar o eliminar elementos de manera correcta
     return *(int*)a == *(int*)b ;
 }
-
-void lecturaEscenarios(Map *escenarios) { //LISTO
-    FILE *archivo = fopen("graphquest.csv", "r") ;
+/* lee cada dato sobre los escenarios desde el archivo "graphquest.csv" y los almacena en un mapa*/
+void lecturaEscenarios(Map *escenarios) { 
+    FILE *archivo = fopen("graphquest.csv", "r") ; //abre el archivo
     if (archivo == NULL) {
         perror("Error al abrir el archivo") ;
         return ;}
 
-    fgets((char[256]){0}, 256, archivo) ;// Leer la cabecera (línea 1) y descartarla
+    fgets((char[256]){0}, 256, archivo) ;// Leer la cabecera (linea 1) y la descarta (ignora en resumen)
     char **campos ;
     while ((campos = leer_linea_csv(archivo, ',')) != NULL)
-    {
+    {//procesa datos de los escenarios
         int id = atoi(campos[0]) ;
         Escenario *nuevoEscenario = malloc(sizeof(Escenario)) ;
         nuevoEscenario->ID = id ;
@@ -86,7 +86,7 @@ void lecturaEscenarios(Map *escenarios) { //LISTO
     printf("                                Escenarios leidos...\n") ;
 }
 
-void mostrarEstadoJugador(Jugador* jugador) {
+void mostrarEstadoJugador(Jugador* jugador) { //muestra la informacion actual del jugador (osea cada vez que hace algun cambio)
     Escenario* escenario = jugador->actual ;
     printf("\n===== ESTADO ACTUAL DEL JUGADOR =====\n") ;
     printf("     Escenario: %s\n", escenario->nombreEscenario) ;
@@ -96,7 +96,7 @@ void mostrarEstadoJugador(Jugador* jugador) {
     if (list_size(jugador->inventario) == 0) {
         printf("     ~~~ No hay items en el inventario ~~~\n") ; 
     }
-    else{
+    else{ //muestra toodos los items que tenga el jugador en el inventario
         for (Item* item = list_first(jugador->inventario) ; item != NULL ; item = list_next(jugador->inventario)) {
             printf(" * Nombre del Item: %s, Peso: %d [kg], Valor: %d [pts]\n", item->nombreItem, item->pesoItem, item->valorItem) ;
         }
@@ -105,7 +105,7 @@ void mostrarEstadoJugador(Jugador* jugador) {
     printf("     ~ Peso total: %d [kg]\n", jugador->pesoTotal) ;
     printf("     ~ Puntaje acumulado: %d [pts]\n\n", jugador->puntajeAcumulado) ;
 
-    printf("     Items disponibles en el escenario:\n") ;
+    printf("     Items disponibles en el escenario:\n") ; //muestra los items del escenario en el momento
     Item* item = list_first(escenario->items) ;
     if (item == NULL) printf("    ...No hay items en este escenario...\n") ;
     else{ //arreglado, ahora imprime todos los items del escenario
@@ -113,7 +113,7 @@ void mostrarEstadoJugador(Jugador* jugador) {
             printf("        * Nombre del Item: %s, Peso: %d [kg], Valor: %d [pts]\n", item->nombreItem, item->pesoItem, item->valorItem) ;
             item = list_next(escenario->items) ;
         }
-    }
+    } //muestra las posibles acciones que puede realizar el jugador
     printf("\nACCIONES DISPONIBLES:\n") ;
     printf(" Movimientos:\n") ;
     if (escenario->arriba != -1) printf("  - Moverse hacia ARRIBA \n") ;
@@ -143,12 +143,12 @@ void recogerItem(Jugador* jugador) { //recoge un item del escenario y lo agrega 
     if (opcion < 1 || opcion > list_size(itemsJugador)) {
         printf("Seleccion invalida...\n") ;
         return ;}
-    int posicion = 1 ;
+    int posicion = 1 ; //agrega el item al inventario y lo elimina del escenario
     for (Item* item =  list_first(itemsJugador) ; item != NULL ; item = list_next(itemsJugador)) {
         if (posicion == opcion) {
             list_pushBack(jugador->inventario, item) ;
-            jugador->pesoTotal += item->pesoItem ;
-            jugador->puntajeAcumulado += item->valorItem ;
+            jugador->pesoTotal += item->pesoItem ; //suma el peso de los objetos (lo mas seguro en kilos)
+            jugador->puntajeAcumulado += item->valorItem ; //suma los valores de los objetos (lo cuento en puntos)
             list_popCurrent(itemsJugador) ;//elimina el item del escenario
             jugador->tiempoRestante -= 1.0 ; //cada item recogido consume 1 (segundo?)
             printf("Haz recogido el item %s\n", item->nombreItem) ;
@@ -163,7 +163,7 @@ void descartarItem(Jugador* jugador) { //descarta un item del inventario del jug
     if (list_size(jugador->inventario) == 0){
         printf("No tienes items en el inventario\n") ;
         return ;}
-    int k = 1 ;
+    int k = 1 ; //muestra los posibles objetos que el jugador puede dejar
     for (Item* item = list_first(jugador->inventario) ; item != NULL ; item = list_next(jugador->inventario)) {
         printf(" %d. Nombre del Item: %s, Peso: %d [kg], Valor: %d [pts]\n", k, item->nombreItem, item->pesoItem, item->valorItem) ;
         k++ ;}
@@ -173,9 +173,9 @@ void descartarItem(Jugador* jugador) { //descarta un item del inventario del jug
     if (opcion < 1 || opcion > list_size(jugador->inventario)) {
         printf("Seleccion invalida...\n") ;
         return ;}
-    int indice = 1 ;
+    int indice = 1 ; //elimina el item que se haya seleccionado
     for (Item* item = list_first(jugador->inventario) ; item != NULL ; item = list_next(jugador->inventario)) {
-        if (indice == opcion) {
+        if (indice == opcion) { //quita el objeto de la lista creada para guardar los objetos en el inventario
             jugador->pesoTotal -= item->pesoItem ;
             jugador->puntajeAcumulado -= item->valorItem ;
             printf("Haz descartado el item %s\n", item->nombreItem) ;
@@ -186,8 +186,8 @@ void descartarItem(Jugador* jugador) { //descarta un item del inventario del jug
     }
     
 }
-
-void moverJugador(Jugador* jugador, Map* grafo, char direccion) {
+//funciona para que el jugador pueda moverse entre escenarios
+void moverJugador(Jugador* jugador, Map* grafo, char direccion) { 
     int idNulo = -1 ;
     if (direccion == 'w') idNulo = jugador->actual->arriba ;
     else if (direccion == 's') idNulo = jugador->actual->abajo ;
@@ -197,7 +197,7 @@ void moverJugador(Jugador* jugador, Map* grafo, char direccion) {
         printf("No puedes moverte en esa direccion\n") ;
         return ;}
     
-    MapPair* parcito = map_first(grafo) ;
+    MapPair* parcito = map_first(grafo) ; //busca el siguiente escenario al que puede ir el jugador
     while (parcito) {
         int* llave = parcito->key ;
         if (*llave == idNulo) {
@@ -207,12 +207,12 @@ void moverJugador(Jugador* jugador, Map* grafo, char direccion) {
             int pesoTotal_aux = 0 ;
             for (Item* item = list_first(jugador->inventario) ; item != NULL ; item = list_next(jugador->inventario)) {
                 pesoTotal_aux += item->pesoItem ;
-            }
+            } //toda esta parte sirve para calcular el tiempo que consume el jugador mientras mas peso lleve encima
             float tiempoConsumido = (pesoTotal_aux + 1) / 10.0 ;
             jugador->tiempoRestante -= tiempoConsumido ; //cada movimiento consume 1 segundo por cada kg de peso total
             printf("Tiempo Consumido : %.1f \n", tiempoConsumido) ;
 
-            if (jugador->actual->esFinal == 1){ //verifica si el escenario es el final
+            if (jugador->actual->esFinal == 1){ //verifica si el jugador llega al escenario final
                 printf(" HAS LLEGADO A LA SALIDA!\n") ;
                 printf("  JUEGO TERMINADO!\n") ;
                 printf("  Tu puntaje final fue de: %d [pts]\n", jugador->puntajeAcumulado) ;
@@ -226,17 +226,17 @@ void moverJugador(Jugador* jugador, Map* grafo, char direccion) {
     printf("No se encontro el escenario....\n") ;
 }  //mueve al jugador a un nuevo escenario
  
-Jugador* crearJugador(Map* grafo) {
+Jugador* crearJugador(Map* grafo) { //crea un nuevo jugador e inicializa su estado, osea que todos sus datos estan en 0
     Jugador* jugador = malloc(sizeof(Jugador)) ;
     jugador->actual = ((MapPair*)map_first(grafo))->value ; //inicia en el primer escenario
-    jugador->inventario = list_create() ;
+    jugador->inventario = list_create() ;//inventario vacio
     jugador->tiempoRestante = 10 ;
     jugador->puntajeAcumulado = 0 ;
     jugador->pesoTotal = 0 ;
     return jugador ;
 }
 
-void liberarMemoria(Jugador* player, Map* grafo){
+void liberarMemoria(Jugador* player, Map* grafo){ //libera toda la momoria usada por el jugador y el grafo
      list_clean(player->inventario) ;
     free(player->inventario) ;
     free(player) ;
@@ -257,18 +257,19 @@ void liberarMemoria(Jugador* player, Map* grafo){
 
 void jugar() { //mete todas las funciones para formar el juego
     printf("                                Cargando escenarios...\n") ;
-    Map* grafo = map_create(compararEnteros) ;
+    //crea el grafo principal que contiene los escenarios usando la funcion de comparacion para IDs
+    Map* grafo = map_create(compararEnteros) ; 
     if (grafo == NULL){
         printf("ERROR EN ARCHIVOS DE LECTURA... INTENTE MAS TARDE") ;
         return ;
     }
-    lecturaEscenarios(grafo) ;
-    Jugador* player = crearJugador(grafo) ;
+    lecturaEscenarios(grafo) ; //carga los escenarios desde el archivo CSV al grafo
+    Jugador* player = crearJugador(grafo) ; //crea al jugador y lo inicializa
 
     int salir = 0 ;
     while (!salir) {
-        mostrarEstadoJugador(player) ;
-        puts("Selecciona una opcion:") ;
+        mostrarEstadoJugador(player) ; //meustra el estado actual del jugador: items, puntaje, ubicacion.. etc
+        puts("Selecciona una opcion:") ; //menu de opciones
         puts("1. Recoger Item") ;
         puts("2. Descartar Item") ;
         puts("3. Avanzar") ;
@@ -279,51 +280,51 @@ void jugar() { //mete todas las funciones para formar el juego
         scanf("%d", &opcion) ;
         switch (opcion) {
             case 1: 
-                system("cls||clear") ;
-                recogerItem(player) ;
+                system("cls||clear") ; //este limpia la pantalla :)
+                recogerItem(player) ; //agrega items del escenario en el que se encuentra el juagdor
                 break ;
             case 2: 
                 system("cls||clear") ;
-                descartarItem(player) ;
+                descartarItem(player) ; //elimina items del inventario
                 break ;
             case 3: 
                 system("cls||clear") ;
                 printf("Indica direccion (w/a/s/d): ") ;
                 char direccion ;
                 scanf(" %c", &direccion) ;
-                moverJugador(player, grafo, direccion) ;
+                moverJugador(player, grafo, direccion) ; //hace que el jugador se pueda mover por el "mapa"
                 break ;
             case 4:
                 system("cls||clear") ;
                 printf("\n ~~~~~ REINICIANDO PARTIDA ~~~~~ \n") ;
-                player = crearJugador(grafo) ;
+                player = crearJugador(grafo) ; //reincia al jugador, todo desde 0
                 break ;
             case 5:
                 system("cls||clear") ;
                 printf("GRACIAS POR JUGAR!. Tu puntaje fue de: %d [pts]\n\n", player->puntajeAcumulado) ;
-                salir = 1 ;
+                salir = 1 ; //hace que termine el juego
                 break ;
             default:
                 system("cls||clear") ;
-                puts("Reintente Nuevamente....") ;
+                puts("Reintente Nuevamente....") ; //por si se selecciona una opcion no valida
         }
-        if (player->tiempoRestante <= 0 ) {
+        if (player->tiempoRestante <= 0 ) { //verifica si se acabo el tiempo
             printf("SE ACABO EL TIEMPO!!!. Fin del Juego\n") ;
             printf("Tu puntaje final fue de: %d [pts]\n\n", player->puntajeAcumulado) ;
             salir = 1 ;
         }
-        if (player->actual->esFinal){
+        if (player->actual->esFinal){ //verifica si se llego al final
             printf("\n ~~~ FELICIDADES! ~~~ . \n") ;
             printf("Llegaste al escenario final\n") ;
             printf("Tu puntaje final fue de: %d [pts]\n\n", player->puntajeAcumulado) ;
             salir = 1 ;
         }
     }
-    liberarMemoria(player, grafo) ;
+    liberarMemoria(player, grafo) ; //libera toda la memoria usada
 }
 
 int main() {
     printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ BIENVENIDO A GRAPHQUEST ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n") ;
-    jugar() ;
-    return 0 ;
+    jugar() ; //inicia el juego
+    return 0 ; //termina el programa
 }
